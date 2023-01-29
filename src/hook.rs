2a533d1597a4cb20bccf43825b93788bf5b737d9
segfo <k.segfo@gmail.com>
2023-01-29 19:10:32 +0900

    :tada: CTRL+Vを無効化するライブラリ。
    クリップボードペーストモード時に使用されます。

---
+++ src/hook.rs
@@ -0,0 +1,48 @@
+
+use windows::{
+    Win32::{
+        Foundation::*,
+        UI::WindowsAndMessaging::*,
+    },
+};
+use std::{ffi::OsString, sync::Mutex};
+use std::os::windows::ffi::OsStringExt;
+use once_cell::unsync::*;
+#[link_section = ".shared"]
+pub static mut hook: HHOOK = HHOOK(0);
+#[link_section = ".shared"]
+pub static mut g_dll: HINSTANCE = HINSTANCE(0);
+#[link_section = ".shared"]
+static mut g_ignore :Lazy<Mutex<isize>>=Lazy::new(||{Mutex::new(0)});
+
+#[no_mangle]
+pub extern "system" fn hook_proc(ncode: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    let vkey=wparam.0;
+    if HC_ACTION as i32 == ncode {
+        let ignore = unsafe{g_ignore.lock().unwrap()};
+        if vkey==162{
+            return LRESULT(*ignore);
+        }
+        if vkey=='V' as usize{
+            return LRESULT(*ignore);
+        }
+    }
+    unsafe { CallNextHookEx(hook, ncode, wparam, lparam) }
+}
+#[no_mangle]
+unsafe extern "C" fn ignore_ctrl_v(){
+    let mut ignore = unsafe{g_ignore.lock().unwrap()};
+    *ignore=1;
+}
+#[no_mangle]
+unsafe extern "C" fn notice_ctrl_v(){
+    let mut ignore = unsafe{g_ignore.lock().unwrap()};
+    *ignore=0;
+}
+
+unsafe fn u16_ptr_to_string(ptr: *const u16) -> OsString {
+    let len = (0..).take_while(|&i| *ptr.offset(i) != 0).count();
+    let slice = std::slice::from_raw_parts(ptr, len);
+
+    OsString::from_wide(slice)
+}
---
+++ src/lib.rs
@@ -0,0 +1,74 @@
+use windows::{
+    core::*,
+    Win32::{
+        Foundation::*,
+        System::{DataExchange::*, Memory::*, SystemServices::*, WindowsProgramming::*},
+        UI::WindowsAndMessaging::*,
+    },
+};
+enum DLL {
+    PROCESS_DETACH = 0,
+    PROCESS_ATTACH = 1,
+    THREAD_ATTACH = 2,
+    THREAD_DETACH = 3,
+}
+impl From<u32> for DLL {
+    fn from(n: u32) -> Self {
+        match n {
+            0 => DLL::PROCESS_DETACH,
+            1 => DLL::PROCESS_ATTACH,
+            2 => DLL::THREAD_ATTACH,
+            _ => DLL::THREAD_DETACH,
+        }
+    }
+}
+
+// https://learn.microsoft.com/ja-jp/windows/win32/dlls/dllmain
+#[no_mangle]
+unsafe extern "system" fn DllMain(h_inst: HINSTANCE, reason: u32, l_param: u32) -> bool {
+    match DLL::from(reason) {
+        DLL::PROCESS_ATTACH => {
+            g_dll = h_inst;
+        }
+        DLL::PROCESS_DETACH => {
+        }
+        DLL::THREAD_ATTACH => {}
+        DLL::THREAD_DETACH => {}
+    }
+    true
+}
+mod hook;
+use hook::*;
+
+#[no_mangle]
+unsafe extern "C" fn sethook() -> bool {
+    unsafe {
+        let dll: HINSTANCE = g_dll;
+        hook = match SetWindowsHookExW(WH_KEYBOARD, Some(hook_proc), dll, 0) {
+            Ok(handle) => handle,
+            Err(_) => {
+                return false;
+            }
+        };
+    }
+    true
+}
+#[no_mangle]
+unsafe extern "C" fn unhook() -> bool {
+    unsafe {
+        if hook.0 != 0 {
+            let r = UnhookWindowsHookEx(hook);
+        }
+    }
+    true
+}
+fn msg(caption: &str, msg: &str) {
+    unsafe {
+        MessageBoxW(
+            HWND::default(),
+            &HSTRING::from(msg),
+            &HSTRING::from(caption),
+            MB_OK,
+        );
+    }
+}
