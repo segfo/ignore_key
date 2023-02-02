b5cdc2e05718d281d31a8a25aac3d25c0b61e384
segfo <k.segfo@gmail.com>
2023-02-02 21:14:16 +0900

    :bug: ペースト機能が無効化されているときにもIMEがオフになってしまう事象を解消した

--- src/hook.rs
+++ src/hook.rs
@@ -17,17 +17,19 @@ pub extern "system" fn hook_proc(ncode: i32, wparam: WPARAM, lparam: LPARAM) -> 
     let vkey = wparam.0;
     if HC_ACTION as i32 == ncode {
         if vkey == 'V' as usize {
-            // CTRL+VならとにかくIMEを無効化する
-            unsafe {
-                let lctrl = GetAsyncKeyState(VK_LCONTROL.0 as i32) as u16 & 0x8000 > 0;
-                if lctrl {
-                    let hwnd = GetForegroundWindow();
-                    let ctx = ImmGetContext(hwnd);
-                    ImmSetOpenStatus(ctx, false);
-                    ImmReleaseContext(hwnd, ctx);
+            let ignore = { *unsafe { IGNORE.read().unwrap() } };
+            if ignore == 1 {
+                // CTRL+VならとにかくIMEを無効化する
+                unsafe {
+                    let lctrl = GetAsyncKeyState(VK_LCONTROL.0 as i32) as u16 & 0x8000 > 0;
+                    if lctrl {
+                        let hwnd = GetForegroundWindow();
+                        let ctx = ImmGetContext(hwnd);
+                        ImmSetOpenStatus(ctx, false);
+                        ImmReleaseContext(hwnd, ctx);
+                    }
                 }
             }
-            let ignore = { *unsafe { IGNORE.read().unwrap() } };
             return LRESULT(ignore);
         }
     }
